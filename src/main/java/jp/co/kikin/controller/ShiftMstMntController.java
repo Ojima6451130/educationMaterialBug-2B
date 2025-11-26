99de23689bb56537395ee4ea654737c25d5c2be7
DESKTOP-IJV0DFO\school02-08 <s.hisano.turquoise@gmail.com>
2025-11-26 11:18:30 +0900

    Revert "No.17『更新』を押下するとエラー画面に遷移 return先を修正（久野)"
    This reverts commit ca0614b91cd32da454dff2212e2ca25b14064c24.

--- src/main/java/jp/co/kikin/controller/ShiftMstMntController.java
+++ src/main/java/jp/co/kikin/controller/ShiftMstMntController.java
@@ -159,8 +159,7 @@ public class ShiftMstMntController {
 
         // 更新後のフォームをリダイレクト先に渡す
         redirectAttributes.addFlashAttribute("shiftMstMntForm", form);
-        //return null;→修正
-        return "shiftMstMnt";
+        return null;
     }
 
 
