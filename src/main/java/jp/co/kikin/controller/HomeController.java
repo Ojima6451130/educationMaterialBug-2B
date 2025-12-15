9c86238b4c69bd08faea7aa68b95ff13f286b81f
DESKTOP-GQOP8UO\school02-09 <m.hosoi.sapphire@gmail.com>
2025-12-15 15:23:37 +0900

    画面のデザインを変更しました！(細井)

--- src/main/java/jp/co/kikin/controller/HomeController.java
+++ src/main/java/jp/co/kikin/controller/HomeController.java
@@ -11,6 +11,7 @@ import jakarta.servlet.http.HttpSession;
 import jp.co.kikin.constant.CommonConstant;
 import jp.co.kikin.constant.RequestSessionNameConstant;
 import jp.co.kikin.dto.DailyShiftDto;
+import jp.co.kikin.dto.LoginDto;
 import jp.co.kikin.service.CommonUtils;
 import jp.co.kikin.service.DailyShiftLogic;
 
@@ -18,11 +19,11 @@ import jp.co.kikin.service.DailyShiftLogic;
 @RequestMapping("/kikin")
 public class HomeController {
 
-    private final UserShiftLogic userShiftLogic;
+	private final UserShiftLogic userShiftLogic;
 
-    HomeController(UserShiftLogic userShiftLogic) {
-        this.userShiftLogic = userShiftLogic;
-    }
+	HomeController(UserShiftLogic userShiftLogic) {
+		this.userShiftLogic = userShiftLogic;
+	}
 
 	@RequestMapping(value = "/home")
 	public String init(HttpSession session, HttpServletRequest request, Model model) throws Exception {
@@ -39,20 +40,25 @@ public class HomeController {
 		// 定数
 		model.addAttribute("userAuthority", CommonConstant.Authority.USER.getId());
 		model.addAttribute("adminAuthority", CommonConstant.Authority.ADMIN.getId());
-		
-		// 今日のシフトセット　長谷川
-		//今日の日付取得
-        String yearMonthDay = CommonUtils.getFisicalDay();
-        //ログインID取得
-		String employeeId = (String)session.getAttribute(RequestSessionNameConstant.SESSION_CMN_LOGIN_USER_ID);
+
+		// 今日のシフトセット 長谷川
+		// 今日の日付取得
+		String yearMonthDay = CommonUtils.getFisicalDay();
+		// ログインID取得
+		String employeeId = (String) session.getAttribute(RequestSessionNameConstant.SESSION_CMN_LOGIN_USER_ID);
 		UserShiftLogic logic = new UserShiftLogic();
-		//一人分の今日のシフト取得
+		// 一人分の今日のシフト取得
 		DailyShiftDto dto = logic.getTodayShift(yearMonthDay, employeeId);
-		model.addAttribute("todayShift",dto);
+		model.addAttribute("todayShift", dto);
 		if (dto == null || dto.getStartTime() == null || dto.getStartTime().isEmpty()) {
 			model.addAttribute("todayShiftMessage", "本日は勤務がありません");
 			model.addAttribute("todayShift", null);
 		}
+
+		String employeeName = (String) session.getAttribute(RequestSessionNameConstant.SESSION_CMN_LOGIN_USER_NAME);
+		model.addAttribute("employeeID", employeeId);
+		model.addAttribute("employeeName", employeeName);
+
 		return "home";
 	}
 
--- src/main/java/jp/co/kikin/prepository/SalaryRecodeRepository.java
+++ src/main/java/jp/co/kikin/prepository/SalaryRecodeRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import jp.co.kikin.pentity.SalaryRecodeEntity;
+import jp.co.kikin.pentity.SalaryRecodeId;
 
 @Repository
-public interface SalaryRecodeRepository extends JpaRepository<SalaryRecodeEntity, String> {
+public interface SalaryRecodeRepository extends JpaRepository<SalaryRecodeEntity, SalaryRecodeId> {
 
 }
